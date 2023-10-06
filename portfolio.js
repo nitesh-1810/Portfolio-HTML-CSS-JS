f51943dfb52e37c70655438934057dc3be7c130b
nitesh-1810 <99216825+nitesh-1810@users.noreply.github.com>
2023-10-07 03:24:33 +0530

    Added icons in Project card 1 & modify Read more button

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+var typeData = new Typed(".role", {
+  strings: ["Frontend Developer", "Web Developer", "Coder"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
+
+//console.log(window.innerWidth);
