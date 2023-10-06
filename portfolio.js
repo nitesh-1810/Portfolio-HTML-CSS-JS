83e0074f2aad9e4e3284ce46f22d9737acb9bad2
nitesh-1810 <99216825+nitesh-1810@users.noreply.github.com>
2023-10-07 03:29:21 +0530

    Read more button made functional.

--- portfolio.js
+++ portfolio.js
@@ -6,4 +6,27 @@ var typeData = new Typed(".role", {
   backDelay: 1000,
 });
 
+var dots = document.getElementsByClassName("dots");
+
+var more = document.getElementsByClassName("more");
+
+var readBtn = document.getElementsByClassName("readMore");
+var skillsIcons = document.getElementsByClassName(
+  "project-skills-container-icons"
+);
+
+const readMore = (id) => {
+  if (more[id].style.display === "none") {
+    readBtn[id].innerText = "Read less";
+    more[id].style.display = "inline";
+    dots[id].style.display = "none";
+    skillsIcons[id].style.display = "none";
+  } else {
+    more[id].style.display = "none";
+    readBtn[id].innerText = "Read more";
+    dots[id].style.display = "inline";
+    skillsIcons[id].style.display = "flex";
+  }
+};
+
 //console.log(window.innerWidth);
