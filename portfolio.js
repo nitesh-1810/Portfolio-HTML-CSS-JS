57234b1ba76914f4d69ca4a8ebc06ca25e142a40
nitesh-1810 <99216825+nitesh-1810@users.noreply.github.com>
2023-11-14 22:52:16 +0530

    Add functionality to contact us form.

--- portfolio.js
+++ portfolio.js
@@ -56,3 +56,26 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID = "service_h4yer8x";
+  const templateID = "template_3rpxka8";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+
+      console.log(res);
+      // alert("your message sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
