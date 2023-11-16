e23f9a8f41e9710190962e078511a15d758473aa
nitesh-1810 <99216825+nitesh-1810@users.noreply.github.com>
2023-11-16 22:10:45 +0530

    Validation added in Contact us form.

--- portfolio.js
+++ portfolio.js
@@ -56,26 +56,55 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+const validation = () => {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+  if (name === "") {
+    console.log("name empty");
+  }
+  if (email === "") {
+    console.log("email empty");
+  }
+  if (subject === "") {
+    console.log("subject empty");
+  }
+  if (message === "") {
+    console.log("message empty");
+  }
+  if (name !== "" && email !== "" && subject !== "" && message !== "") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    subject: document.getElementById("subject").value,
-    message: document.getElementById("message").value,
-  };
-  const serviceID = "service_h4yer8x";
-  const templateID = "template_3rpxka8";
+  const valid = validation();
 
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("subject").value = "";
-      document.getElementById("message").value = "";
+  if (valid) {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+    const serviceID = "service_h4yer8x";
+    const templateID = "template_3rpxka8";
 
-      console.log(res);
-      // alert("your message sent successfully");
-    })
-    .catch((err) => console.log(err));
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+
+        console.log(res);
+        // alert("your message sent successfully");
+      })
+      .catch((err) => console.log(err));
+  }
 }
